050e21ca287829d01f4f48f7d90ba3ffcf67dc9d
Wallace <wallacefcardoso@gmail>
2020-06-15 16:48:42 -0300

    users example

---
+++ backend/java/api/admin/src/main/java/com/wafec/api/admin/services/UserService.java
@@ -0,0 +1,10 @@
+package com.wafec.api.admin.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public String sayHello() {
+        return "Hello!";
+    }
+}
